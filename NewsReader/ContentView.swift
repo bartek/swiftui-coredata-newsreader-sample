6d2acb5908427c18abbc4d40fefcaa74be387a6d
Bartek Ciszkowski <bart.ciszk@gmail.com>
2020-08-03 20:38:02 -0400

    Initial Commit

---
+++ NewsReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsReader
+//
+//  Created by Bartek Ciszkowski on 2020-08-03.
+//  Copyright © 2020 Bartek Ciszkowski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
